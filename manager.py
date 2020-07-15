6187616f6c1b6e33a98690b35c34a4ccf9a5b3f7
DengZY123 <33028325+DengZY123@users.noreply.github.com>
2020-07-15 11:06:12 +0800

    Update manager.py

--- manager.py
+++ manager.py
@@ -8,7 +8,7 @@ from application import app
 import www
 
 #manager.add_command("runserver",Server(port=app.config['SERVER_PORT'],use_debugger=True,use_reloader=True))
-manager.add_command("runserver",Server(port=8999,use_debugger=True ,use_reloader=True))
+manager.add_command("runserver",Server(port=8000,use_debugger=True ,use_reloader=True))
 
 def main():
     manager.run()
